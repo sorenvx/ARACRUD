9a36ad5cee836ab0528377b20c0fdca5792c5675
Toni De Gea <sorenvx@hotmail.com>
2020-07-17 18:40:45 +0200

    Initial Commit

---
+++ ARACRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARACRUD
+//
+//  Created by Toni De Gea on 17/07/2020.
+//  Copyright © 2020 Toni De Gea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
